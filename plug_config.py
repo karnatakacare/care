9344f237b386c7db760ebdbc492557fb4ca095d3
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2025-11-27 03:08:50 +0530

    Add multiple plugins to plug_config.py

--- plug_config.py
+++ plug_config.py
@@ -1,5 +1,40 @@
 from plugs.manager import PlugManager
+from plugs.plug import Plug
 
-plugs = []
+abdm_plugin = Plug(
+    name="abdm",
+    package_name="git+https://github.com/10bedicu/care_abdm.git",
+    version="@production",
+    configs={}
+)
+
+
+gateway_device_plugin = Plug(
+    name="gateway_device",
+    package_name="git+https://github.com/10bedicu/care_teleicu_devices.git",
+    version="@main",
+    configs={}
+)
+
+camera_device_plugin = Plug(
+    name="camera_device",
+    package_name="git+https://github.com/10bedicu/care_teleicu_devices.git",
+    version="@main",
+    configs={}
+)
+
+vitals_observation_device_plugin = Plug(
+    name="vitals_observation_device",
+    package_name="git+https://github.com/10bedicu/care_teleicu_devices.git",
+    version="@main",
+    configs={}
+)
+
+plugs = [
+    abdm_plugin,
+    gateway_device_plugin,
+    camera_device_plugin,
+    vitals_observation_device_plugin
+]
 
 manager = PlugManager(plugs)
